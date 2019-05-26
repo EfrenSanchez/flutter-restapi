bc1ec894c33e37a428cd7f5e484b38c40f8db35a
Efren Sanchez <efren.sanchezgon@gmail.com>
2019-05-26 17:26:22 +0200

    Update database.js

--- server/src/database.js
+++ server/src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    await mongoose.connect('mongodb://efren:efren123@ds261136.mlab.com:61136/flutter-restapi', {
+    await mongoose.connect('mongodb://db:pass@ds261136.mlab.com:61136/flutter-restapi', {
         useNewUrlParser: true
     });
     console.log('Database: Connected');
